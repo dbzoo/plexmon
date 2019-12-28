7f06cf51ff4caa9f84ff4d4de386015b85071af2
dbzoo <brett@dbzoo.com>
2019-12-28 16:49:51 +0000

    Fix extra quote

--- get_token.py
+++ get_token.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
 from plexapi.myplex import MyPlexAccount
 account = MyPlexAccount('<user name>','<password>')
-plex = account.resource('<plex server name>'').connect()
+plex = account.resource('<plex server name>').connect()
 print plex._token
